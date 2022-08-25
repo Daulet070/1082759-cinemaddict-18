3d95f6ef8f3f7e63183ccb2521a6c8b1a2cee1e3
Daulet <51912820+Daulet070@users.noreply.github.com>
2022-08-26 01:03:11 +0300

    Added: raw data for film card object

---
+++ src/mock/raw-data.js
@@ -0,0 +1,119 @@
+const FILMS_COUNT = 5;
+const COMMENTS_COUNT = 5;
+
+const moviesRating = {
+  MIN: 1,
+  MAX: 9
+};
+
+const moviesDuration = {
+  MIN: 20,
+  MAX: 180
+};
+
+const descriptions = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.'
+];
+
+const titles = [
+  'Terminator 2',
+  'Blade Runner',
+  'Evil Dead',
+  'Harry Potter',
+  'Forrest Gump'
+];
+
+const posters = [
+  'made-for-each-other.png',
+  'popeye-meets-sinbad.png',
+  'sagebrush-trail.jpg',
+  'santa-claus-conquers-the-martians.jpg',
+  'the-dance-of-life.jpg',
+  'the-great-flamarion.jpg',
+  'the-man-with-the-golden-arm.jpg'
+];
+
+const genres = [
+  'Comedy',
+  'Action',
+  'Thriller',
+  'Western',
+  'Cartoon'
+];
+
+const directors = [
+  'Tom Ford',
+  'Piter Jackson',
+  'Eldar Ryazanov',
+  'Steven Spielberg',
+  'Ridley Scott'
+];
+
+const actors = [
+  'Arnold Schwarzenegger',
+  'Johnny Depp',
+  'Jack Nicholson',
+  'Cortney Cox',
+  'Angelina Jolie'
+];
+
+const ageRatings = [
+  '0+',
+  '3+',
+  '16+'
+];
+
+const countries = [
+  'USA',
+  'France',
+  'Russia',
+  'China',
+  'India',
+  'Finland'
+];
+
+const writers = [
+  'Takeshi Kitano',
+  'Johnny Depp',
+  'Jack Nicholson',
+  'Cortney Cox'
+];
+
+const commentAutors = [
+  'Michael',
+  'Sarah',
+  'John',
+  'Zoe',
+  'Maria'
+];
+
+const commentEmotions = [
+  'smile',
+  'sleeping',
+  'puke',
+  'angry'
+];
+
+export {
+  FILMS_COUNT,
+  COMMENTS_COUNT,
+  ageRatings,
+  moviesRating,
+  moviesDuration,
+  titles,
+  posters,
+  descriptions,
+  directors,
+  actors,
+  countries,
+  commentAutors,
+  commentEmotions,
+  genres,
+  writers
+};

f1cd3c462509b7d4594c5b2b9299316fa4866aed
Daulet <51912820+Daulet070@users.noreply.github.com>
2022-08-29 14:22:49 +0300

    Added: film model, main presenter, popup presenter

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import Presenter from './presenter/presenter';
+import FilmModel from './model/film-model';
+import FilmDetailsPopupPresenter from './presenter/film-details-popup-presenter';
+import MainContentPresenter from './presenter/main-content-presenter';
 import { render } from './render';
 import StatisticsView from './view/movies-statistics';
 import ProfileView from './view/user-profile';
@@ -8,9 +10,14 @@ const mainContent = document.querySelector('.main');
 const footerContent = document.querySelector('.footer');
 const statistics = footerContent.querySelector('.footer__statistics');
 
+const filmsModel = new FilmModel();
+
 render(new ProfileView(), headerContent);
 render(new StatisticsView(), statistics);
 
-const mainPresenter = new Presenter();
+const mainContentPresenter = new MainContentPresenter();
+const filmDetailsPopupPresenter = new FilmDetailsPopupPresenter();
 
-mainPresenter.init(mainContent);
+mainContentPresenter.init(mainContent, filmsModel);
+filmDetailsPopupPresenter.init(filmsModel);
+// console.log(filmsModel.getFilms());
